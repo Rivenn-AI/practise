ea3ddbd00ea951e279dc5838cecd1a439bb297d7
Rivenn-AI <jalolbekatamurodov@gmail.com>
2026-09-12 09:45:36 +0900

    Python special dunders

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
-
+# Dunder -- __buildins__, __init__
+a = 100
+print("a=", a)
+message = "Hello world"
+print(message)
+# Pythonda hamma narsa object xisoblanadi
+result = type(message)
+# message1 = "string qiymat olamiz"
+print("message:", result)
+''' In Python, builin tools:
+(1) TYPES > int float str list dict
+(2) Function > print() len() input() type() str() int()
+(3) Constanta > True False None
+'''
+# print(dir(__builtins__))
