34fb3a545bb8800983d438e141dc6b8c0894e429
Rivenn-AI <jalolbekatamurodov@gmail.com>
2026-09-12 14:00:26 +0900

    Task.js

---
+++ functions.py
@@ -0,0 +1,45 @@
+''' Functions
+(1) Define and call
+(2) Parametr va argument
+(3) Keyword va default arguments
+(4) scope
+'''
+print("====Define <Parametr> and Call <Argument>")
+# build in function > print() type()
+# Function - reuseable block of code
+# Instead of block { } in Java, Python uses Indentation
+
+
+# define
+def greet(a):
+    print(f"How do you do, {a}")
+
+
+def greeting(b):
+    print("greeting is executed")
+    return f"hi {b}"
+
+
+# Call - execute
+result1 = greet("Martin")
+print("result1:", result1)
+
+result2 = greeting("Justin")
+print("result2", result2)
+
+print("======Keyword and default arguments")
+
+# define
+
+
+def give_great(name, age=22):
+    print("give_great is executed")
+    return f"Hi {name} your age {age} years old"
+
+
+# call >> keyboard arguments
+result3 = give_great(name="Justin", age=20)
+print("result3", result3)
+
+result4 = give_great(name="Justin")
+print("result3", result4)
