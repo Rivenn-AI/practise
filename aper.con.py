6419155d09c06cf71564255f9da85d31d894ddb1
Rivenn-AI <jalolbekatamurodov@gmail.com>
2026-09-15 00:00:34 +0900

    Conditions and Logical operators

--- aper.con.py
+++ aper.con.py
@@ -33,3 +33,41 @@ print(id(c), id(d), id(c))
 
 print("c is d", c is d)
 print("c is e", c is e)
+
+
+print("=====Conditions====")
+
+x = 15
+if x > 50:
+    print("case A")
+elif x > 10:
+    print("case B")
+else:
+    print("case C")
+print("=====Logical Operators====")
+# logical operators
+age = 10
+# person = None
+# if age > 16:
+#     person = "adult"
+# else:
+#     person = "child"
+
+# print("person:", person)
+
+# Ternery
+person = "adult" if age > 18 else "minor"
+print("person", person)
+
+is_student = True
+is_admin = False
+is_guest = True
+is_parent = True
+if not is_student:  # not operator
+    print("Student bulish xoxlayszmi")
+elif is_admin:
+    print("please go to this university ")
+elif is_guest or is_parent:  # bittasi true olsa true olib ketadi. Agar and bulsa bitta xato bulsa false qaytaradi
+    print("Witing is over there")
+else:
+    print("other cases")
