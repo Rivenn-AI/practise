cf5489f86b2ab68d16bc32810ae567f60c84aba6
Rivenn-AI <jalolbekatamurodov@gmail.com>
2026-09-14 12:34:17 +0900

    feat: Dictionar and Iterable obj

---
+++ Iterable.py
@@ -0,0 +1,37 @@
+print("====Iterable Objects & Range======")
+# Iterable objects >String dict tuple list range map filter
+
+
+# ------Range-----
+range_obj = range(3)
+print("Range qiymat", range_obj)
+# ------- string----
+text = "MIT-A9"
+for letter in text:
+    print(f"the letter: {letter}")
+for ele in range_obj:
+    print(f"the element: {ele}")
+
+
+print("=====Dictionary====")
+# Dictioanry is JSON object;
+person = {"name": "Ali", "age": 22, "single": True}
+person_obj = dict(name="Riven", age=25, single=True)
+
+print(f"the person:{person}")
+print(f"the person_obj{person_obj}")
+
+name = person_obj["name"]
+print("name:", name)
+
+# name = person_obj["hobby"]
+# print("nmae", name)
+name = person_obj.get("name")
+hobby = person_obj.get("hobby", )
+balance = person_obj.get("balance", 0)
+print(f"the name:{name} hobby {hobby} and balnce is {balance}")
+
+
+del person_obj["single"]  # del bu biz xoxlamagan stateni uchrish uchun
+for key in person_obj:
+    print(f"key:{key} value=> {person_obj[key]}")
